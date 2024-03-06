d5348828b700c3bdbb8eeb3c6f6059c5ecc89ea2
Fernando Menchaca <fmenchacav@gmail.com>
2024-03-06 13:37:11 -0600

    First commit. Basic implementation of ADC single conversion driver, using uart comm to transmit data to serial COM

---
+++ Debug/Src/uart.d
@@ -0,0 +1,18 @@
+Src/uart.o: ../Src/uart.c ../Inc/uart.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/core_cm4.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/cmsis_version.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/cmsis_compiler.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/cmsis_gcc.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/mpu_armv7.h \
+ C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/uart.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/core_cm4.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/cmsis_version.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/cmsis_compiler.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/cmsis_gcc.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Include/mpu_armv7.h:
+C:/Users/fmenc/Desktop/STM32F4_Workspace/chip_headers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
--- README.md
+++
@@ -1,2 +0,0 @@
-# ADC_Single_Convertion
-An ADC single convertion driver for an STM32F411RE with an UART implementation to send converted data to serial port on a computer
